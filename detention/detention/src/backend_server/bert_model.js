0382d9627f3c9744a3608d98db32eb41f4a35374
trey_weathers <treychelle.weathers2017@gmail.com>
2023-05-20 10:50:01 -0400

    Update bert_model.js

--- detention/detention/src/backend_server/bert_model.js
+++ detention/detention/src/backend_server/bert_model.js
@@ -6,22 +6,28 @@ import '@tensorflow/tfjs-backend-cpu';
 import * as qna from "@tensorflow-models/qna";
 //const qna = require('@tensorflow-models/qna');
 
-console.log ('Using TensorFlow backend: ', tf.getBackend ());
+console.log ('Using TensorFlow backend!'); tf.getBackend ();
 //loadAndPredict (); - load and predict not defined
 // Adds the CPU backend to the global backend registry.
 
-// Load the model.
-const model = qna.load();
-console.log("model is loaded!")
-
-//model = await qna.load();
+//const model = qna.load();
+//console.log("model is loaded!")
 
 //EXAMPLE
-const passage = "Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet's leading subsidiary and will continue to be the umbrella company for Alphabet's Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet."
 const question = "Who is the CEO of Google?"
+const passage = "Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet's leading subsidiary and will continue to be the umbrella company for Alphabet's Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet."
 
-// Finding the answers
-const answers = model.findAnswers(question, passage);
+
+async function predict(){
+  // Load the model.
+  const model = await qna.load();
+  // Finding the answers
+  const answers = await model.findAnswers(question, passage);
+  console.log(answers);
+}
+predict();
+
+
 /*
 [{
   text: "Sundar Pichai",
@@ -30,8 +36,6 @@ const answers = model.findAnswers(question, passage);
   score: 0.8380282521247864
 }]
 */
-//console.log('Answers: ');
-console.log(answers);
 
 export default function BertModel() {
   return (
