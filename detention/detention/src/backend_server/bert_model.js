0f7c7fcc72b22cc7206d7f3231a718d790eb7fee
trey_weathers <treychelle.weathers2017@gmail.com>
2023-05-20 16:26:49 -0400

    using fetch to get model API, error free but no parameters to findAnswer b/c not function error

--- detention/detention/src/backend_server/bert_model.js
+++ detention/detention/src/backend_server/bert_model.js
@@ -4,38 +4,29 @@
 import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-backend-cpu';
 import * as qna from "@tensorflow-models/qna";
-//const qna = require('@tensorflow-models/qna');
-
-console.log ('Using TensorFlow backend!'); tf.getBackend ();
-//loadAndPredict (); - load and predict not defined
 // Adds the CPU backend to the global backend registry.
+console.log ('Using TensorFlow backend!'); tf.getBackend ();
+// loadAndPredict (); load and predict not defined
 
-//const model = qna.load();
-//console.log("model is loaded!")
-
-//EXAMPLE
+//test run
 const question = "Who is the CEO of Google?"
 const passage = "Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet's leading subsidiary and will continue to be the umbrella company for Alphabet's Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet."
 
+const model = fetch(qna.load)
+  .then(response => {
+    console.log(`${response}`)
+    console.log('success')
+  })
+  .catch(error => {
+    console.log(`${error}`)
+    console.log("error")
+  })
 
-async function predict(){
-  // Load the model.
-  const model = await qna.load();
-  // Finding the answers
-  const answers = await model.findAnswers(question, passage);
-  console.log(answers);
-}
-predict();
+//model.findAnswers is not a function, removed () - no error but NEED TO PASS IN PARAMATERS
+const answers = model.findAnswers
+//(question, passage)
+console.log(answers)
 
-
-/*
-[{
-  text: "Sundar Pichai",
-  startIndex: 1143,
-  endIndex: 1156,
-  score: 0.8380282521247864
-}]
-*/
 
 export default function BertModel() {
   return (
