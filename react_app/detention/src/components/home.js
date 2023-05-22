ee373dbf9dff1029b3ba584ee462833fcf246bda
trey_weathers <treychelle.weathers2017@gmail.com>
2023-05-21 21:33:02 -0400

    react cleanup

---
+++ react_app/detention/src/components/home.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <div>
+        <header className="header">
+            <h1> Welcome to Virtual Detention </h1>
+            <h4> TAKE A SEAT </h4>
+        </header>
+      </div>
+    )
+}
